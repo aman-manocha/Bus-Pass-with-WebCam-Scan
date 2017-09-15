20f74773ba98659d01c0eb5db0b784a16f56d7df
aman-manocha <amanmanocha060@gmail.com>
2017-09-15 15:03:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,28 @@ class Window(QtGui.QMainWindow):
         self.setGeometry(10,30,1300,700)
         self.setWindowTitle("Bus Pass System")
         self.setWindowIcon(QtGui.QIcon('bus_icon.png'))
+        self.home()
+        
+    def home(self):
+        btn = QtGui.QPushButton("Admin Login", self)
+        btn2 = QtGui.QPushButton("User Login", self)
+        btn3 = QtGui.QPushButton("Logout", self)
+
+        #btn.resize(100,50)
+        btn.move(500,500)
+        
+        #btn2.resize(100,50)
+        btn2.move(700,500)
+
+        #btn3.resize(100,50)
+        btn3.move(1100,50)
+
+        btn3.clicked.connect(QtCore.QCoreApplication.instance().quit)
         self.show()
-
-app = QtGui.QApplication(sys.argv)
-
-GUI = Window()
-sys.exit(app.exec_())
+        
+def run():
+    app = QtGui.QApplication(sys.argv)
+    GUI = Window()
+    sys.exit(app.exec_())
+    
+run()
